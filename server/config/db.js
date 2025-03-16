02f986414a5370069663ed56d025774ff0dc4b85
theinfinity1410 <swayamgosavi1410@gmail.com>
2025-03-16 15:31:23 +0530

    db.js err solved

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import {Sequelize} from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   dialect: 'postgres',
